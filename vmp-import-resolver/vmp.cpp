fb14efeb017b0f99d3fdb1b6e5bbdef37482e95a
noahswtf <131519316+noahswtf@users.noreply.github.com>
2025-01-03 00:22:48 +0000

    Updated stub assembler.
    Updated stub assembler.

--- vmp-import-resolver/vmp.cpp
+++ vmp-import-resolver/vmp.cpp
@@ -99,7 +99,7 @@ void vmp::map_sections(const std::vector<std::pair<std::uintptr_t, std::vector<s
 	}
 }
 
-std::vector<std::uint8_t> get_stub_assembler(std::uint32_t stack_offset)
+std::vector<std::uint8_t> get_stub_assembler(std::uint64_t import_to_call, std::uint32_t stack_offset)
 {
 	asmjit::JitRuntime jit_runtime;
 	asmjit::CodeHolder code_holder;
@@ -107,10 +107,8 @@ std::vector<std::uint8_t> get_stub_assembler(std::uint32_t stack_offset)
 	code_holder.init(jit_runtime.environment());
 	asmjit::x86::Assembler assembler(&code_holder);
 
-	assembler.mov(asmjit::x86::r10, asmjit::x86::rcx);
 	assembler.add(asmjit::x86::rsp, asmjit::imm(stack_offset));
-	assembler.syscall();
-	assembler.ret();
+	assembler.jmp(import_to_call);
 
 	std::uint8_t* jit = nullptr;
 
@@ -139,7 +137,7 @@ void vmp::process_import_calls(const std::vector<std::uintptr_t>& import_calls, 
 
 	portable_executable::image_t* binary_image = reinterpret_cast<portable_executable::image_t*>(dumped_binary.data());
 
-	std::uint64_t stub_size = get_stub_assembler(8).size();
+	std::uint64_t stub_size = get_stub_assembler(binary_image->as<std::uintptr_t>(), 8).size();
 
 	std::uint64_t stub_section_size = stub_size * user_data.stubs_needed.size();
 
